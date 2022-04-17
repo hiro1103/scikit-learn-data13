43f5dd420c1caa9b5f94dafa78af250f31a3ca4a
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-17 23:30:04 +0900

    Add files via upload

--- data13.py
+++ data13.py
@@ -1,9 +1,12 @@
+from send2trash import TrashPermissionError
+from sklearn import datasets
 import tensorflow as tf
 import numpy as np
 import pathlib
 import warnings
 import matplotlib.pyplot as plt
 import os
+import tensorflow_datasets as tfds
 warnings.simplefilter('ignore')
 
 imgdir_path = pathlib.Path('cat_dog_images')
@@ -23,3 +26,125 @@ for i, file in enumerate(file_list):
 
 plt.tight_layout()
 plt.show()
+
+labels = [1 if 'dog' in os.path.basename(file) else 0 for file in file_list]
+print(labels)
+
+ds_file_labels = tf.data.Dataset.from_tensor_slices((file_list, labels))
+for item in ds_file_labels:
+    print(item[0].numpy(), item[1].numpy())
+
+
+def load_and_preprocess(path, label):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [img_height, img_width])
+    image /= 255.0
+    return image, label
+
+
+img_width, img_height = 120, 80
+ds_images_labels = ds_file_labels.map(load_and_preprocess)
+
+fig = plt.figure(figsize=(10, 5))
+for i, example in enumerate(ds_images_labels):
+    ax = fig.add_subplot(2, 3, i+1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(example[0])
+    ax.set_title('{}'.format(example[1].numpy()), size=15)
+
+plt.tight_layout()
+plt.show()
+print(len(tfds.list_builders()))
+print(tfds.list_builders()[:5])
+
+celaba_bldr = tfds.builder('celeb_a')
+print(celaba_bldr.info.features)
+print(celaba_bldr.info.features['image'])
+print(celaba_bldr.info.features['attributes'].keys())
+print(celaba_bldr.info.citation)
+mnist, mnist_info = tfds.load('mnist', with_info=True, shuffle_files=False)
+print(mnist_info)
+print(mnist.keys())
+ds_train = mnist['train']
+ds_train = ds_train.map(lambda item: (item['image'], item['label']))
+ds_train = ds_train.batch(10)
+batch = next(iter(ds_train))
+print(batch[0].shape, batch[1])
+
+fig = plt.figure(figsize=(15, 6))
+for i, (image, label) in enumerate(zip(batch[0], batch[1])):
+    ax = fig.add_subplot(2, 5, i+1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(image[:, :, 0], cmap='gray_r')
+    ax.set_title('{}'.format(label), size=15)
+
+plt.show()
+
+X_train = np.arange(10).reshape((10, 1))
+y_train = np.array([1.0, 1.3, 3.1, 2.0, 5.0, 6.3, 6.6, 7.4, 8.0, 9.0])
+plt.plot(X_train, y_train, 'o', markersize=10)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+X_train_norm = (X_train-np.mean(X_train)/np.std(X_train))
+ds_train_orig = tf.data.Dataset.from_tensor_slices(
+    (tf.cast(X_train_norm, tf.float32), tf.cast(y_train, tf.float32))
+)
+
+
+class MyModel(tf.keras.Model):
+    def __init__(self) -> None:
+        super(MyModel, self).__init__()
+        self.w = tf.Variable(0.0, name='weight')
+        self.b = tf.Variable(0.0, name='bias')
+
+    def call(self, x):
+        return self.w*x+self.b
+
+
+model = MyModel()
+model.build(input_shape=(None, 1))
+model.summary()
+
+
+def loss_fn(y_true, y_pred):
+    return tf.reduce_mean(tf.square(y_true-y_pred))
+
+
+def train(model, inputs, outputs, learning_rate):
+    with tf.GradientTape() as tape:
+        current_loss = loss_fn(model(inputs), outputs)
+    dW, db = tape.gradient(current_loss, [model.w, model.b])
+    model.w.assign_sub(learning_rate*dW)
+    model.b.assign_sub(learning_rate*db)
+
+
+tf.random.set_seed(1)
+num_epochs = 200
+log_steps = 100
+learning_rate = 0.001
+batch_size = 1
+steps_per_epoch = int(np.ceil(len(y_train)/batch_size))
+
+ds_train = ds_train_orig.shuffle(buffer_size=len(y_train))
+ds_train = ds_train.repeat(count=None)
+ds_train = ds_train.batch(1)
+Ws, bs = [], []
+
+for i, batch in enumerate(ds_train):
+    if i >= steps_per_epoch*num_epochs:
+        break
+    Ws.append(model.w.numpy())
+    bs.append(model.b.numpy())
+
+    bx, by = batch
+    loss_val = loss_fn(model(bx), by)
+
+    train(model, bx, by, learning_rate=learning_rate)
+    if i % log_steps == 0:
+        print('Epoch {:4d} step {:2d} Loss {:6.4f}'.format(
+            int(i/steps_per_epoch), i, loss_val))
