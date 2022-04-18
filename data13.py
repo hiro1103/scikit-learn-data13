4c1d6a07280ba15fff6d2a36dbe3c7838c2f019b
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-18 09:11:48 +0900

    Add files via upload

--- data13.py
+++ data13.py
@@ -1,5 +1,4 @@
-from send2trash import TrashPermissionError
-from sklearn import datasets
+from scipy.special import expit
 import tensorflow as tf
 import numpy as np
 import pathlib
@@ -193,6 +192,163 @@ plt.legend(['Weight w', 'Bias unit b'], fontsize=15)
 ax.set_xlabel('Iteration', size=15)
 ax.set_ylabel('Value', size=15)
 ax.tick_params(axis='both', which='major', labelsize=15)
-# plt.savefig('ch13-linreg-1.pdf')
-
 plt.show()
+
+tf.random.set_seed(1)
+model = MyModel()
+model.compile(optimizer='sgd',
+              loss=loss_fn,
+              metrics=['mae', 'mse'])
+
+model.fit(X_train_norm, y_train,
+          epochs=num_epochs, batch_size=batch_size, verbose=1)
+
+iris, iris_info = tfds.load('iris', with_info=True)
+print(iris_info)
+
+tf.random.set_seed(1)
+ds_orig = iris['train']
+ds_orig = ds_orig.shuffle(150, reshuffle_each_iteration=False)
+ds_train_orig = ds_orig.take(100)
+ds_test = ds_orig.skip(100)
+
+ds_train_orig = ds_train_orig.map(lambda x: (x['features'], x['label']))
+ds_test = ds_test.map(lambda x: (x['features'], x['label']))
+
+iris_model = tf.keras.Sequential([
+    tf.keras.layers.Dense(16, activation='sigmoid',
+                          name='fc1', input_shape=(4,)),
+    tf.keras.layers.Dense(3, name='fc2', activation='softmax')
+
+])
+
+iris_model.summary()
+
+iris_model.compile(optimizer='adam',
+                   loss='sparse_categorical_crossentropy',
+                   metrics=['accuracy'])
+
+num_epochs = 100
+training_size = 100
+batch_size = 2
+steps_per_epoch = np.ceil(training_size / batch_size)
+
+ds_train = ds_train_orig.shuffle(buffer_size=training_size)
+ds_train = ds_train.repeat()
+ds_train = ds_train.batch(batch_size=batch_size)
+ds_train = ds_train.prefetch(buffer_size=1000)
+
+
+history = iris_model.fit(ds_train, epochs=num_epochs,
+                         steps_per_epoch=steps_per_epoch,
+                         verbose=0)
+hist = history.history
+
+fig = plt.figure(figsize=(12, 5))
+ax = fig.add_subplot(1, 2, 1)
+ax.plot(hist['loss'], lw=3)
+ax.set_title('Training loss', size=15)
+ax.set_xlabel('Epoch', size=15)
+ax.tick_params(axis='both', which='major', labelsize=15)
+
+ax = fig.add_subplot(1, 2, 2)
+ax.plot(hist['accuracy'], lw=3)
+ax.set_title('Training accuracy', size=15)
+ax.set_xlabel('Epoch', size=15)
+ax.tick_params(axis='both', which='major', labelsize=15)
+plt.tight_layout()
+plt.show()
+
+results = iris_model.evaluate(ds_test.batch(50), verbose=0)
+print('Test loss: {:.4f} Test Acc.: {:.4f}'.format(*results))
+
+iris_model.save('iris-classifier.h5',
+                overwrite=True,
+                include_optimizer=True,
+                save_format='h5')
+
+iris_model_new = tf.keras.models.load_model('iris-classifier.h5')
+
+results = iris_model_new.evaluate(ds_test.batch(50), verbose=0)
+print('Test loss: {:.4f} Test Acc.:{:.4f}'.format(*results))
+
+X = np.array([1, 1.4, 2.5])
+w = np.array([0.4, 0.3, 0.5])
+
+
+def net_input(X, w):
+    return np.dot(X, w)
+
+
+def logistic(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def logistic_activation(X, w):
+    z = net_input(X, w)
+    return logistic(z)
+
+
+print('P(y=1|x) = %.3f' % logistic_activation(X, w))
+
+# この配列の最初の列(W[:][0])はバイアスユニット
+W = np.array([[1.1, 1.2, 0.8, 0.4],
+             [0.2, 0.4, 1.0, 0.2],
+             [0.6, 1.5, 1.2, 0.7]])
+
+A = np.array([[1, 0.1, 0.4, 0.6]])
+
+Z = np.dot(W, A[0])
+y_probas = logistic(Z)
+print('Net Input: \n', Z)
+print('Output units:\n', y_probas)
+
+y_class = np.argmax(Z, axis=0)
+print('Predicted class label: %d' % y_class)
+
+
+def softmax(z):
+    return np.exp(z)/np.sum(np.exp(z))
+
+
+y_probas = softmax(Z)
+print('Probabilities:\n', y_probas)
+
+np.sum(y_probas)
+
+Z_tensor = tf.expand_dims(Z, axis=0)
+tf.keras.activations.softmax(Z_tensor)
+
+
+def tanh(z):
+    e_p = np.exp(z)
+    e_m = np.exp(-z)
+    return (e_p-e_m)/(e_p+e_m)
+
+
+z = np.arange(-5, 5, 0.005)
+log_act = logistic(z)
+tanh_act = tanh(z)
+plt.ylim([-1.5, 1.5])
+plt.xlabel('net input $z$')
+plt.ylabel('activation $\phi(z)$')
+plt.axhline(1, color='black', linestyle=':')
+plt.axhline(0.5, color='black', linestyle=':')
+plt.axhline(0, color='black', linestyle=':')
+plt.axhline(-0.5, color='black', linestyle=':')
+plt.axhline(-1, color='black', linestyle=':')
+plt.plot(z, tanh_act, linewidth=3, linestyle='--', label='tanh')
+plt.plot(z, log_act, linewidth=3, label='logstic')
+plt.legend(loc='lower right')
+plt.tight_layout()
+plt.show()
+
+np.tanh(z)
+
+tf.keras.activations.tanh(z)
+
+expit(z)
+
+tf.keras.activations.sigmoid(z)
+
+tf.keras.activations.relu(z)
